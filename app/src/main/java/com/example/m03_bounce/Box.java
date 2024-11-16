2f2bb33d718fef0aa17492e4fb87feb4c34ac4bc
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2024-11-16 14:10:21 -0400

    Reapply "Updated codebase to better align with ASOP"
    This reverts commit 4ea94094f063bd8c11a0df30aee2619c012deb5d.

--- app/src/main/java/com/example/m03_bounce/Box.java
+++ app/src/main/java/com/example/m03_bounce/Box.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2024 Nick Richardson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.m03_bounce;
 
 import android.content.Context;
@@ -5,11 +21,12 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 
 import androidx.core.content.ContextCompat;
 
 /**
- * Created by Russ on 08/04/2014.
+ * The Box class represents the boundaries of the game area.
  */
 public class Box {
 
@@ -17,11 +34,29 @@ public class Box {
     private Drawable backgroundDrawable;
     private Rect bounds;
 
+    /**
+     * Constructor for the Box class.
+     *
+     * @param context The context of the application
+     * @param drawableId The ID of the drawable resource to use as the background
+     */
     public Box(Context context, int drawableId) {
-        backgroundDrawable = ContextCompat.getDrawable(context, drawableId);
+        try {
+            backgroundDrawable = ContextCompat.getDrawable(context, drawableId);
+        } catch (Exception e) {
+            Log.e("Box", "Error loading background drawable." + drawableId, e);
+        }
         bounds = new Rect();
     }
 
+    /**
+     * Sets the bounds of the box.
+     *
+     * @param x The x-coordinate of the top-left corner of the box
+     * @param y The y-coordinate of the top-left corner of the box
+     * @param width The width of the box
+     * @param height The height of the box
+     */
     public void set(int x, int y, int width, int height) {
         xMin = x;
         xMax = x + width - 1;
@@ -34,6 +69,11 @@ public class Box {
         }
     }
 
+    /**
+     * Draws the box on the canvas.
+     *
+     * @param canvas The canvas to draw the box on
+     */
     public void draw(Canvas canvas) {
         if(backgroundDrawable != null) {
 
--- app/src/main/java/com/example/m03_bounce/Coin.java
+++ app/src/main/java/com/example/m03_bounce/Coin.java
@@ -1,12 +1,37 @@
+/*
+ * Copyright (C) 2024 Nick Richardson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.m03_bounce;
 
 import android.content.Context;
 
+/**
+ * The Coin class represents a coin falling object in the game.
+ */
 public class Coin extends FallingObject{
 
     private static final int drawableId = R.drawable.ic_coin;
     private static final int value = 5;
 
+    /**
+     * Constructor for the Coin class.
+     *
+     * @param context The context of the application
+     * @param gameView The GameView object that the coin will be placed in
+     */
     public Coin(Context context, GameView gameView) {
         super(context, drawableId, value, gameView);
     }
